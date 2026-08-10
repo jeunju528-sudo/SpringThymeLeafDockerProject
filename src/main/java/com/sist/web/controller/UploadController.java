ecd61c0ac7f8b8f692c582db2dd8bfd562ee32cc
jeunju528_sudo <228353885+jeunju528-sudo@users.noreply.github.com>
2026-08-10 11:23:26 +0900

    feat: jquery ver multi upload

--- src/main/java/com/sist/web/controller/UploadController.java
+++ src/main/java/com/sist/web/controller/UploadController.java
@@ -1,17 +1,36 @@
 package com.sist.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UploadController {
 	@GetMapping("/upload")
-	public String upload_page() {
-		return "upload";
+	public String upload_page(Model model) {
+		
+		model.addAttribute("main_html", "upload");
+		return "main/main";
 	}
 	
+	@GetMapping("/upload2")
+	public String upload_page2(Model model) {
+		
+		model.addAttribute("main_html", "upload2");
+		return "main/main";
+	}
+	
+	@GetMapping("/upload3")
+	public String upload_page3(Model model) {
+		
+		model.addAttribute("main_html", "upload3");
+		return "main/main";
+	}
+	
+	/*
 	@GetMapping("/upload2")
 	public String upload_page2() {
 		return "upload2";
 	}
+	*/
 }
